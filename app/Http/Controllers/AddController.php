bb3ce6471dc165a0a4be77a960fe2d0cc935c3de
Abdoosama123 <ao3689971@gmail.com>
2022-10-30 13:29:46 +0200

    first commit

---
+++ app/Http/Controllers/AddController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddController extends Controller
+{
+    public function index1 (){
+        return 'namespace' ;
+    }
+
+
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Book::select('*');
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+
+                    $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('users');
+    }}
